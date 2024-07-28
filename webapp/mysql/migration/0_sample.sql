6e9559cb20d16d4e9acdd09de9544d0adb742d4e
pokotieru-mashumaro <k827umeboshi@gmail.com>
2024-07-28 09:43:04 +0900

    iikanzi

--- webapp/mysql/migration/0_sample.sql
+++ webapp/mysql/migration/0_sample.sql
@@ -1,15 +1,25 @@
 -- このファイルに記述されたSQLコマンドが、マイグレーション時に実行されます。
 
+-- users
 CREATE INDEX idx_username ON users(username);
+
+-- sessions
+create index sessions_session_token_index on sessions (session_token);
+
+-- orders
+CREATE INDEX idx_node_id on orders (node_id);
+
+-- dispatchers
 CREATE INDEX idx_user_id ON dispatchers(user_id);
 
-create index sessions_session_token_index on sessions (session_token);
--- CREATE INDEX idx_tt_status_area_id ON tow_trucks (status, area_id);
-CREATE INDEX idx_tow_trucks_status ON tow_trucks(status);
-
--- CREATE INDEX idx_node_a_id ON edges (node_a_id);
-CREATE INDEX idx_edges_covering ON edges (node_a_id, node_b_id, weight);
--- CREATE INDEX idx_completed_orders_order_id ON completed_orders (order_id);
+-- completed_orders
 CREATE INDEX idx_co_covering ON completed_orders (order_id, id, tow_truck_id, completed_time);
 
+-- edges
+CREATE INDEX idx_node_a_id on edges(node_a_id);
+
+-- locations
 CREATE INDEX idx_locations_truck_time ON locations (tow_truck_id, timestamp DESC);
+
+-- nodes
+CREATE INDEX idx_nodes_area ON nodes (area_id);
